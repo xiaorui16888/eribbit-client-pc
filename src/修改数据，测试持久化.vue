f619f2bf0cac51cc114a02e8b021de8e26218166
xiaorui16888 <87920151@qq.com>
2022-06-16 14:23:40 +0800

    Create 修改数据，测试持久化.vue

---
+++ src/修改数据，测试持久化.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <!-- 修改数据，测试是否持久化 -->
+    App {{$store.state.user.profile.account}}
+    <br>
+    <button @click="$store.commit('user/setUser',{account:'xiaorui'})">设置用户信息</button>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'App',
+  setup () {
+    return {
+    }
+  }
+}
+
+</script>
