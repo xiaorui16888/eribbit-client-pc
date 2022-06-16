9ba309271a8119031d75f39637af439398252d0d
xiaorui16888 <87920151@qq.com>
2022-06-16 14:46:31 +0800

    测试request工具函数

---
+++ src/测试request工具函数.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <button @click="fn">测试request工具函数</button>
+  </div>
+</template>
+<script>
+import request from '@/utils/request'
+export default {
+  name: 'App',
+  setup () {
+    const fn = () => {
+      // 测试请求
+      request('/member/profile', 'get', { a: 10 })
+    }
+
+    return {
+      fn
+    }
+  }
+}
+
+</script>
