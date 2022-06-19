40b9a50a03b9155f0538a995c23a82dd349d15bb
xiaorui16888 <87920151@qq.com>
2022-06-19 23:35:35 +0800

    吸顶头部-传统方法和组合API的实现

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,10 +1,13 @@
 <template>
     <!-- 顶部头栏 -->
     <AppNavBar/>
+    <!-- 吸顶头部 -->
+    <!-- <AppHeaderSticky/> -->
+    <AppHeaderStickyNew/>
     <!-- 头部组件 -->
     <AppHeader/>
     <!-- 内容容器 -->
-    <div class="main">
+    <div class="main" style="height: 500px;">
       <!-- 二级路由 -->
       <RouterView/>
     </div>
@@ -15,13 +18,17 @@
 import AppNavBar from '@/components/app-navbar'
 import AppHeader from '@/components/app-header'
 import AppFooter from '@/components/app-footer'
+// import AppHeaderSticky from '@/components/app-header-sticky'
+import AppHeaderStickyNew from '@/components/app-header-sticky-new'
 import { useStore } from 'vuex'
 export default {
   name: 'LayoutBase',
   components: {
     AppNavBar,
     AppHeader,
-    AppFooter
+    AppFooter,
+    // AppHeaderSticky
+    AppHeaderStickyNew
   },
   setup () {
     const store = useStore()
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,19 +1,18 @@
 <template>
-    <div class="container">
-        HomePage
+  <div class="page-home">
+    <div class="home-entry">
+      <div class="container">
+        <!-- 左侧分类 -->
+        <HomeCategory />
+      </div>
     </div>
+  </div>
 </template>
 <script>
+import HomeCategory from '@/components/home-category'
 export default {
-  name: 'HomePage'
+  name: 'PageHome',
+  components: { HomeCategory }
 }
 </script>
-<style scoped lang="less">
-// @import '../../assets/styles/mixins.less';
-// @import '../../assets/styles/variables.less';
-
-.container{
-    background: @xtxColor;
-    .hoverShadow();
-}
-</style>
+<style scoped lang="less"></style>
