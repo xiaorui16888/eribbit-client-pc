2544a244634d750da1b4126b5d4f0a53cf8d288f
xiaorui16888 <87920151@qq.com>
2022-06-20 18:11:40 +0800

    首页-主体-新鲜好物

--- src/api/home.js
+++ src/api/home.js
@@ -1,13 +1,21 @@
 import request from '@/utils/request'
 // 获取品牌
-export const findBrand = (limit) => {
+export const findBrand = (limit = 6) => {
   return request('/home/brand', 'get', { limit })
 }
 
 /**
- * 获取广告图
+ * 获取广告区轮播图
  * @returns Promise
  */
 export const findBanner = () => {
   return request('/home/banner', 'get')
+}
+
+/**
+ * 获取新鲜好物
+ * @returns Promise
+ */
+export const findNew = () => {
+  return request('/home/new', 'get')
 }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -6,12 +6,7 @@
         <HomeCategory />
         <!-- 轮播图 -->
         <HomeBanner />
-        <!-- 新鲜好物 -->
-        <HomePanel title="新鲜好物" sub-title="新鲜出炉 品质靠谱">
-          <template #right>
-            <XtxMore path="/"></XtxMore>
-          </template>
-        </HomePanel>
+        <HomeNew/>
       </div>
     </div>
   </div>
@@ -19,15 +14,13 @@
 <script>
 import HomeCategory from './components/home-category'
 import HomeBanner from './components/home-banner'
-import HomePanel from './components/home-panel'
-import XtxMore from '@/components/library/xtx-more.vue'
+import HomeNew from './components/home-new'
 export default {
   name: 'PageHome',
   components: {
     HomeCategory,
     HomeBanner,
-    HomePanel,
-    XtxMore
+    HomeNew
   }
 }
 </script>
