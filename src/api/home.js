ef01de401e674f95b7a9a27d8a0b47c53e890e1d
xiaorui16888 <87920151@qq.com>
2022-06-20 17:31:33 +0800

    首页-轮播图模块实现

--- src/api/home.js
+++ src/api/home.js
@@ -1,4 +1,13 @@
 import request from '@/utils/request'
+// 获取品牌
 export const findBrand = (limit) => {
   return request('/home/brand', 'get', { limit })
+}
+
+/**
+ * 获取广告图
+ * @returns Promise
+ */
+export const findBanner = () => {
+  return request('/home/banner', 'get')
 }
