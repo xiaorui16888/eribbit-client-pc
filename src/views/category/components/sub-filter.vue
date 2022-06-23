fdca91040bbd8652d77174074e37c1bd84978e5b
xiaorui16888 <87920151@qq.com>
2022-06-23 15:57:37 +0800

    二级类目-筛选区展示

---
+++ src/views/category/components/sub-filter.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="sub-filter" v-if="filterData && !filterLoading">
+    <div class="item">
+      <div class="head">品牌：</div>
+      <div class="body">
+        <a :class="{active:filterData.selectedBrand===brand.id}" href="javasript:;" v-for="brand in filterData.brands" :key="brand.id">{{brand.name}}</a>
+      </div>
+    </div>
+    <div class="item" v-for="p in filterData.saleProperties" :key="p.id">
+      <div class="head">{{p.name}}：</div>
+      <div class="body">
+        <a :class="{active:p.selectedProp===attr.id}" href="javasript:;" v-for="attr in p.properties" :key="attr.id">{{attr.name}}</a>
+      </div>
+    </div>
+  </div>
+  <div v-else class="sub-filter">
+    <XtxSkeleton class="item" width="800px" height="40px"  />
+    <XtxSkeleton class="item" width="800px" height="40px"  />
+    <XtxSkeleton class="item" width="600px" height="40px"  />
+    <XtxSkeleton class="item" width="600px" height="40px"  />
+    <XtxSkeleton class="item" width="600px" height="40px"  />
+  </div>
+</template>
+<script>
+import { findSubCategoryFilter } from '@/api/category'
+import { useRoute } from 'vue-router'
+import { ref, watch } from 'vue'
+export default {
+  name: 'SubFilter',
+  setup () {
+    // 1. 获取数据
+    // 2. 数据中需要全部选中，需要预览将来点击激活功能。默认选中全部
+    // 3. 完成渲染
+    const route = useRoute()
+    const filterData = ref(null)
+    const filterLoading = ref(false)
+    // 4. 分类发生变化的时候需要重新获取筛选数据，需要使用侦听器
+    watch(() => route.params.id, (newVal, oldVal) => {
+      // 当你从二级分类去顶级分类也会拿到ID，不能去加载数据因为它不是二级分类的ID
+      if (newVal && route.path === ('/category/sub/' + newVal)) {
+        filterLoading.value = true
+        newVal && findSubCategoryFilter(route.params.id).then(({ result }) => {
+        // 品牌全部
+          result.selectedBrand = null
+          result.brands.unshift({ id: null, name: '全部' })
+          // 销售属性全部
+          result.saleProperties.forEach(p => {
+            p.selectedProp = null
+            p.properties.unshift({ id: null, name: '全部' })
+          })
+          filterData.value = result
+          filterLoading.value = false
+        })
+      }
+    }, { immediate: true })
+    return { filterData, filterLoading }
+  }
+}
+</script>
+<style scoped lang='less'>
+ .xtx-skeleton {
+      padding: 10px 0;
+    }
+  // 筛选区
+  .sub-filter {
+    background: #fff;
+    padding: 25px;
+    .item {
+      display: flex;
+      line-height: 40px;
+      .head {
+        width: 80px;
+        color: #999;
+      }
+      .body {
+        flex: 1;
+        a {
+          margin-right: 36px;
+          transition: all .3s;
+          display: inline-block;
+          &.active,
+          &:hover {
+            color: @xtxColor;
+          }
+        }
+      }
+    }
+  }
+</style>
--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -3,15 +3,20 @@
     <div class="container">
       <!-- 面包屑 -->
       <SubBread />
-
+      <!-- 筛选区 -->
+      <SubFilter/>
     </div>
   </div>
 </template>
 
 <script>
 import SubBread from './components/sub-bread'
+import SubFilter from './components/sub-filter'
 export default {
   name: 'SubCategory',
-  components: { SubBread }
+  components: {
+    SubBread,
+    SubFilter
+  }
 }
 </script>
