882028076ec6036b8c24396c721e8d367c0afeb8
xiaorui16888 <87920151@qq.com>
2022-06-25 12:04:48 +0800

    复选框组件的封装
    使用useVModel实现复选框-父子组件之间的通讯

---
+++ src/components/library/xtx-checkbox.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="xtx-checkbox" @click="changeChecked()">
+        <i v-if="checked" class="iconfont icon-checked"></i>
+        <i v-else class="iconfont icon-unchecked"></i>
+        <span v-if="$slots.default">
+            <slot />
+        </span>
+    </div>
+</template>
+<script>
+import { useVModel } from '@vueuse/core'
+
+export default {
+  name: 'SubFilter',
+  props: {
+    modelValue: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup (props, { emit }) {
+    // 使用useVModel实现数据双向绑定
+    // 1.使用props接受modelValue
+    // 2.使用useVModel来包装props中的modelValue属性数据
+    // 3.再使用checked.value就是使用父组件数据
+    // 4.再使用checked.value='数据'赋值，触发emit('update:modelValue','数据')
+    const checked = useVModel(props, 'modelValue', emit)
+    const changeChecked = () => {
+      const newVal = !checked.value
+      // 通知父组件
+      checked.value = newVal
+      // 让组件支持change事件
+      emit('change', newVal)
+    }
+    return { checked, changeChecked }
+  }
+}
+</script>
+<style scoped lang='less'>
+.xtx-checkbox {
+    display: inline-block;
+    margin-right: 2px;
+
+    .icon-checked {
+        color: @xtxColor;
+
+        ~span {
+            color: @xtxColor;
+        }
+    }
+
+    i {
+        position: relative;
+        top: 1px;
+    }
+
+    span {
+        margin-left: 2px;
+    }
+}
+</style>
--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -5,6 +5,8 @@
       <SubBread />
       <!-- 筛选区 -->
       <SubFilter/>
+      <!-- 测试复选框 -->
+      <XtxCheckbox v-model="isAllChecked">全选</XtxCheckbox>{{isAllChecked}}
     </div>
   </div>
 </template>
@@ -12,11 +14,18 @@
 <script>
 import SubBread from './components/sub-bread'
 import SubFilter from './components/sub-filter'
+import XtxCheckbox from '@/components/library/xtx-checkbox'
+import { ref } from 'vue'
 export default {
   name: 'SubCategory',
   components: {
     SubBread,
-    SubFilter
+    SubFilter,
+    XtxCheckbox
+  },
+  setup () {
+    const isAllChecked = ref(false)
+    return { isAllChecked }
   }
 }
 </script>
