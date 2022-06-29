24a80d5b087d3f9fc7d6419986dd23a385925012
xiaorui16888 <87920151@qq.com>
2022-06-29 09:58:52 +0800

    二级分类-无限数据加载及加载动画

--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -12,6 +12,13 @@
         <!-- 排序 -->
         <SubSort />
         <!-- 列表 -->
+        <ul>
+          <li v-for="goods in goodList" :key="goods.id" >
+            <GoodsItem :goods="goods" />
+          </li>
+        </ul>
+        <!-- 无限加载组件 -->
+        <XtxInfiniteLoading :loading="loading" :finished="finished" @infinite="getData"/>
       </div>
     </div>
   </div>
@@ -20,27 +27,86 @@
 <script>
 import SubBread from './components/sub-bread'
 import SubFilter from './components/sub-filter'
-// import XtxCheckbox from '@/components/library/xtx-checkbox'
 import SubSort from './components/sub-sort'
-import { ref } from 'vue'
+import GoodsItem from './components/goods-item'
+import { ref, watch } from 'vue'
+import XtxInfiniteLoading from '@/components/library/xtx-infinite-loading.vue'
+import { findSubCategoryGoods } from '@/api/category'
+import { useRoute } from 'vue-router'
 export default {
   name: 'SubCategory',
   components: {
     SubBread,
     SubFilter,
-    // XtxCheckbox,
-    SubSort
+    SubSort,
+    GoodsItem,
+    XtxInfiniteLoading
   },
   setup () {
     const isAllChecked = ref(false)
-    return { isAllChecked }
+    const route = useRoute()
+
+    // 是否正在加载
+    const loading = ref(false)
+    // 是否加载完毕
+    const finished = ref(false)
+    // 商品列表数据
+    const goodList = ref([])
+    // 请求参数
+    let reqParams = {
+      page: 1,
+      pageSize: 20
+    }
+
+    const getData = () => {
+      loading.value = true
+      console.log('加载数据')
+      // 设置二级分类的ID
+      reqParams.categoryId = route.params.id
+      findSubCategoryGoods(reqParams).then(({ result }) => {
+        // 获取数据成功
+        if (result.items.length) {
+          // 有数据就追加数据
+          goodList.value.push(...result.items)
+          // 把page改成下一页页码
+          reqParams.page++
+        } else {
+          // 没有数据
+          finished.value = false
+        }
+        loading.value = false
+      })
+    }
+
+    // 在更改了二级分类的时候需要重新加载数据
+    watch(() => route.params.id, (newVal) => {
+      if (newVal && `/category/sub${newVal}` === route.path) {
+        goodList.value = []
+        reqParams = { page: 1, pageSize: 20 }
+        finished.value = false
+      }
+    })
+
+    return { isAllChecked, getData, loading, finished, goodList }
   }
 }
 </script>
-<style scoped lang="less">
+<style scoped lang='less'>
 .goods-list {
   background: #fff;
   padding: 0 25px;
   margin-top: 25px;
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0 5px;
+    li {
+      margin-right: 20px;
+      margin-bottom: 20px;
+      &:nth-child(5n) {
+        margin-right: 0;
+      }
+    }
+  }
 }
 </style>
