d8509d8e08d141ecd64bbc9fd3e0d7886ebbfb1f
xiaorui16888 <87920151@qq.com>
2022-07-01 09:49:58 +0800

    商品详情基本布局

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ const Layout = () => import('@/views/Layout')
 const Home = () => import('@/views/home')
 const TopCategory = () => import('@/views/category')
 const SubCategory = () => import('@/views/category/sub')
+const Goods = () => import('@/views/goods/index')
 
 const routes = [
   // 一级路由布局容器
@@ -13,7 +14,8 @@ const routes = [
     children: [
       { path: '/', component: Home },
       { path: '/category/:id', component: TopCategory },
-      { path: '/category/sub/:id', component: SubCategory }
+      { path: '/category/sub/:id', component: SubCategory },
+      { path: '/product/:id', component: Goods }
     ]
   }
 ]
