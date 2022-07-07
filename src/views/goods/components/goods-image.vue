8ab3fd64fb3af1ed99e02312ac20434ff9239db3
xiaorui16888 <87920151@qq.com>
2022-07-07 09:55:48 +0800

    商品详情-图片放大镜的实现

--- src/views/goods/components/goods-image.vue
+++ src/views/goods/components/goods-image.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="goods-image">
-    <div class="middle">
+    <!-- 大图 -->
+    <div v-show="show" class="large" :style="[{ backgroundImage: `url(${images[currIndex]})` },largePosition]"></div>
+    <!-- 中图 -->
+    <div class="middle" ref="target">
       <img :src="images[currIndex]" alt="">
+      <!-- 遮罩色块 -->
+      <div v-show="show" class="layer" :style="layerPosition"></div>
     </div>
+    <!-- 小图 -->
     <ul class="small">
-      <li v-for="(img,i) in images" :key="img" :class="{active:i===currIndex}">
-        <img @mouseenter="currIndex=i" :src="img" alt="">
+      <li v-for="(img, i) in images" :key="img" :class="{ active: i === currIndex }">
+        <img @mouseenter="currIndex = i" :src="img" alt="">
       </li>
     </ul>
   </div>
 </template>
 <script>
-import { ref } from 'vue'
+import { useMouseInElement } from '@vueuse/core'
+import { reactive, ref, watch } from 'vue'
 export default {
   name: 'GoodsImage',
   props: {
@@ -23,8 +30,46 @@ export default {
   setup (props) {
     // 当前预览图的索引
     const currIndex = ref(0)
-    return { currIndex }
+
+    // 1.是否显示遮罩和大图
+    const show = ref(false)
+    // 2.遮罩的坐标(样式)
+    const layerPosition = reactive({
+      left: 0,
+      top: 0
+    })
+    // 3.大图的背景定位（样式）
+    const largePosition = reactive({
+      backgroundPositionX: 0,
+      backgroundPositionY: 0
+    })
+    // 4.使用useMouseInElement得到基于元素左上角的坐标和是否离开元素数据
+    const target = ref(null)
+    const { elementX, elementY, isOutside } = useMouseInElement(target)
+
+    watch([elementX, elementY, isOutside], () => {
+      // 5.据得到的数据设置样式数据和是否显示数据
+      // 控制X轴方向的定位 0-200 之间
+      if (elementX.value < 100) layerPosition.left = 0
+      else if (elementX.value > 300) layerPosition.left = 200
+      else layerPosition.left = elementX.value - 100
+      // 控制Y轴方向的定位 0-200 之间
+      if (elementY.value < 100) layerPosition.top = 0
+      else if (elementY.value > 300) layerPosition.top = 200
+      else layerPosition.top = elementY.value - 100
+      // 设置大背景的定位
+      largePosition.backgroundPositionX = -layerPosition.left * 2 + 'px'
+      largePosition.backgroundPositionY = -layerPosition.top * 2 + 'px'
+      // 设置遮罩容器的定位
+      layerPosition.left = layerPosition.left + 'px'
+      layerPosition.top = layerPosition.top + 'px'
+      // 设置是否显示预览大图
+      show.value = !isOutside.value
+    })
+
+    return { currIndex, show, largePosition, layerPosition, target }
   }
+
 }
 </script>
 <style scoped lang="less">
@@ -33,20 +78,47 @@ export default {
   height: 400px;
   position: relative;
   display: flex;
+  z-index: 500;
+
+  .large {
+    position: absolute;
+    top: 0;
+    left: 412px;
+    width: 400px;
+    height: 400px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    background-repeat: no-repeat;
+    background-size: 800px 800px;
+    background-color: #f8f8f8;
+  }
+
   .middle {
     width: 400px;
     height: 400px;
-    background: #f5f5f5;
+    position: relative;
+    cursor: move;
+    .layer {
+      width: 200px;
+      height: 200px;
+      background: rgba(0, 0, 0, .2);
+      left: 0;
+      top: 0;
+      position: absolute;
+    }
   }
+
   .small {
     width: 80px;
+
     li {
       width: 68px;
       height: 68px;
       margin-left: 12px;
       margin-bottom: 15px;
       cursor: pointer;
-      &:hover,&.active {
+
+      &:hover,
+      &.active {
         border: 2px solid @xtxColor;
       }
     }
